f6014a7757e952b758a1e16177a2556b9eae3600
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-05-03 11:57:40 +0300

    very minor code coverage config attempt

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -6,6 +6,6 @@ dotnet tools/sonar/SonarScanner.MSBuild.dll begin /o:"myarichuk" /k:"myarichuk_S
 echo "Running build..."
 dotnet build
 echo "Running tests..."
-dotnet test --logger:trx
+dotnet test --logger:trx --collect "DotnetCodeCoverage"
 echo "Executing MSBuild DLL end command..."
 dotnet tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
