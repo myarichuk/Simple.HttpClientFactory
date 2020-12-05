cacda6741926c66c3bf53d7901886c36832aa0ee
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-12-06 01:32:52 +0200

    adjust CI script to use SonarScanner global CLI tool attempt 3

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Executing SonarScanner begin command..."
-dotnet sonarscanner begin /o:"myarichuk" /k:"myarichuk_Simple.HttpClientFactory" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.cs.opencover.reportsPaths="./Simple.HttpClientFactory.Tests/*.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet tool run dotnet-sonarscanner begin /o:"myarichuk" /k:"myarichuk_Simple.HttpClientFactory" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.cs.opencover.reportsPaths="./Simple.HttpClientFactory.Tests/*.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build
 echo "Running tests..."
 dotnet test --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 echo "Executing SonarScanner end command..."
-dotnet sonarscanner end /d:sonar.login=${SONAR_TOKEN}
+dotnet tool run dotnet-sonarscanner end /d:sonar.login=${SONAR_TOKEN}
