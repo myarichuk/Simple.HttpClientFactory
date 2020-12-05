fc8c96d1bfca76b34c64940cec761f0136c3c2b8
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-12-06 01:13:02 +0200

    adjust CI script to use SonarScanner global CLI tool

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.0.4.24009/sonar-scanner-msbuild-5.0.4.24009-net5.0.zip
-echo "Unzipping..."
-unzip -qq sonar.zip -d tools/sonar
-echo "Displaying file structure..."
-find .
-ls -l tools/sonar
-echo "Changing permissions..."
-chmod +x tools/sonar/sonar-scanner-4.4.0.2170/bin/sonar-scanner
+dotnet tool install --global dotnet-sonarscanner
