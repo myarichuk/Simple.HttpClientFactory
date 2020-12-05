4c3b0c388d262c75397daabaabecf99cb10d2aa5
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-12-06 00:51:37 +0200

    adjust CI script to install SDK and not runtime - attempt 4

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.8.0.12008/sonar-scanner-msbuild-4.8.0.12008-netcoreapp3.0.zip
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.0.4.24009/sonar-scanner-msbuild-5.0.4.24009-net5.0.zip
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Displaying file structure..."
 find .
 ls -l tools/sonar
 echo "Changing permissions..."
-chmod +x tools/sonar/sonar-scanner-4.2.0.1873/bin/sonar-scanner
+chmod +x tools/sonar/sonar-scanner-5.0.4.24009/bin/sonar-scanner
