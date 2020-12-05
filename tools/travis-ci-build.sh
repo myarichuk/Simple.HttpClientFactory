1805591519390ab4e38bceedd8f2ddb75965b522
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-12-06 00:25:13 +0200

    adjust CI script to install SDK and not runtime - attempt 2

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y dotnet-sdk-3.1
+
 echo "Executing MSBuild DLL begin command..."
 dotnet tools/sonar/SonarScanner.MSBuild.dll begin /o:"myarichuk" /k:"myarichuk_Simple.HttpClientFactory" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.cs.opencover.reportsPaths="./Simple.HttpClientFactory.Tests/*.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
