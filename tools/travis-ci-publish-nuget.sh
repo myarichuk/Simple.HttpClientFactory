06d8c9df3a87e01b8ebe028624a402a86ee73153
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-12-06 12:29:05 +0200

    Update travis-ci-publish-nuget.sh

--- tools/travis-ci-publish-nuget.sh
+++ tools/travis-ci-publish-nuget.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-echo "Generating NuGet package"
-dotnet pack --configuration Release -o nupkg -p:Version="$GitVersion_FullSemVer" ./Simple.HttpClientFactory/Simple.HttpClientFactory.csproj 
+if [ $TRAVIS_BRANCH == "master"]; then
+  echo "Generating NuGet package";
+  dotnet pack --configuration Release -o nupkg -p:Version="$GitVersion_FullSemVer" ./Simple.HttpClientFactory/Simple.HttpClientFactory.csproj;
 
-echo "Publishing to 'api.nuget.org'"
-find . -name *.nupkg -type f -print0 | xargs -0 -I pkg dotnet nuget push pkg -k ${NUGET_API_KEY} -s "https://api.nuget.org/v3/index.json" --skip-duplicate
+  echo "Publishing to 'api.nuget.org'";
+  find . -name *.nupkg -type f -print0 | xargs -0 -I pkg dotnet nuget push pkg -k ${NUGET_API_KEY} -s "https://api.nuget.org/v3/index.json" --skip-duplicate;
+else 
+  echo "Skip nuget publish because we are not on 'master branch'!"; 
+fi
